f84388fe41cc11f7a97d6c90a9d28a5a7472b6af
Jeremy Camus <jeremy.camus@reign.cl>
2023-01-15 00:00:39 -0300

    feat: react router context config has been implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+
 const App = () => {
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    <Routes>
+      <Route
+      path="/"
+        element={
+          <div>
+            <h1>Hello World!</h1>
+          </div>
+        }
+      />
+    </Routes>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,18 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import Layout from "./components/layout/Layout";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Layout>
-      <App />
-    </Layout>
+    <BrowserRouter>
+      <Layout>
+        <App />
+      </Layout>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
