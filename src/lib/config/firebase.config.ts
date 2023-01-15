71773a16a7a8251327196e6ef7e5f137ca7fb634
Jeremy Camus <jeremy.camus@reign.cl>
2023-01-14 23:45:25 -0300

    feat: fireabse auth has been added

--- src/lib/config/firebase.config.ts
+++ src/lib/config/firebase.config.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -12,3 +13,4 @@ const firebaseConfig = {
 
 export const app = initializeApp(firebaseConfig);
 export const fireStore = getFirestore();
+export const firebaseAuth = getAuth(app);
