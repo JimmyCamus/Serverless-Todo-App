d3fa04c9691a451b453541fcccc840b87a641bb0
Jimmy Camus <jeremy.camus.varela@gmail.com>
2023-01-26 15:22:00 -0300

    feat: tab icons has been added

--- src/components/teams/TeamNavigation.tsx
+++ src/components/teams/TeamNavigation.tsx
@@ -1,16 +1,20 @@
 import { Link } from "react-router-dom";
+import PersonalIcon from "../icons/PersonalIcon";
+import TeamIcon from "../icons/TeamIcon";
 
 const TeamNavigation = ({ activeTab }: { activeTab: 1 | 2 }) => {
   return (
     <div className="tabs">
       <Link to="/" className={`tab ${activeTab === 1 ? "tab-active" : null}`}>
-        Tab 1
+        <PersonalIcon />
+        <span>Personal</span>
       </Link>
       <Link
         to="/teams"
         className={`tab ${activeTab === 2 ? "tab-active" : null}`}
       >
-        Tab 2
+        <TeamIcon />
+        Team
       </Link>
     </div>
   );
